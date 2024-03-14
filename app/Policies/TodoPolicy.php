f6f622bcfa79ab339060e3a2caab70a6646cb6bf
Arnold Supriyadi <arnoldsupriyadi@Arnold-Macbook.local>
2024-03-14 20:51:02 +0700

    belajar udah selesai

--- app/Policies/TodoPolicy.php
+++ app/Policies/TodoPolicy.php
@@ -8,6 +8,10 @@ use Illuminate\Auth\Access\Response;
 
 class TodoPolicy
 {
+    public function before(User $user, string $ability)
+    {
+        return true;
+    }
     /**
      * Determine whether the user can view any models.
      */
